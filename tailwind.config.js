12f114509ce1374cade689d054b246fd4e851431
Ayush Senapati <ayushsenapati123@gmail.com>
2022-04-14 18:38:32 +0530

    filled content in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+//! here we can add any customisation
+// ./ means current directory and *.html means any html file. So this config file will look and watch for any html file in thr root and it is gonna compile anything thats present in the static style sheet.
+
 module.exports = {
-  content: [],
+  content: ['./*.html'],
   theme: {
     extend: {},
   },
